6fb8dfadddff4c05c45fa44999af6dd73941f154
killian563 <91839079+killian563@users.noreply.github.com>
2023-03-01 11:55:42 +0200

    add new functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const BOARD_SIZE = 9;
+const NUMBER_OF_MINES = 15;
 
 let board = [];
 
@@ -18,4 +19,32 @@ function createBoard() {
   }
 }
 
+function plantMines() {
+  let plantedMines = 0;
+  while (plantedMines < NUMBER_OF_MINES) {
+    const x = Math.floor(Math.random() * BOARD_SIZE);
+    const y = Math.floor(Math.random() * BOARD_SIZE);
+    if (!board[x][y].mine) {
+      board[x][y].mine = true;
+      plantedMines++;
+    }
+  }
+}
 
+function calculateNeighborMines() {
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    for (let j = 0; j < BOARD_SIZE; j++) {
+      if (!board[i][j].mine) {
+        let count = 0;
+        for (let x = i - 1; x <= i + 1; x++) {
+          for (let y = j - 1; y <= j + 1; y++) {
+            if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE && board[x][y].mine) {
+              count++;
+            }
+          }
+        }
+        board[i][j].neighborMines = count;
+      }
+    }
+  }
+}
